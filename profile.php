2725c1fa81d78d70bfa8c78975d2e81fef32c3d9
Shakir Selemani <141034088+shakirjuma@users.noreply.github.com>
2023-09-06 03:40:18 -0300

    Add files via upload

--- profile.php
+++ profile.php
@@ -1,25 +1,30 @@
 <?php
     session_start();
     include('../includes/dbconn.php');
+    date_default_timezone_set('America/Chicago');
     include('../includes/check-login.php');
     check_login();
+    $aid=$_SESSION['id'];
+    if(isset($_POST['update']))
+    {
 
-    if(isset($_POST['update'])){
-        $email=$_POST['emailid'];
-        $aid=$_SESSION['id'];
-        $udate=date('Y-m-d');
-        $query="UPDATE admin set email=?,updation_date=? where id=?";
-        $stmt = $mysqli->prepare($query);
-        $rc=$stmt->bind_param('ssi',$email,$udate,$aid);
-        $stmt->execute();
-        echo"<script>alert('Email id has been successfully updated');</script>";
+    $fname=$_POST['fname'];
+    $mname=$_POST['mname'];
+    $lname=$_POST['lname'];
+    $gender=$_POST['gender'];
+    $contactno=$_POST['contact'];
+    $udate = date('d-m-Y h:i:s', time());
+    $query="UPDATE  userRegistration set firstName=?,middleName=?,lastName=?,gender=?,contactNo=?,updationDate=? where id=?";
+    $stmt = $mysqli->prepare($query);
+    $rc=$stmt->bind_param('ssssisi',$fname,$mname,$lname,$gender,$contactno,$udate,$aid);
+    $stmt->execute();
+    echo"<script>alert('Profile updated Succssfully');</script>";
     }
-
 ?>
 
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
-
+<!-- By CodeAstro - codeastro.com -->
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -33,11 +38,19 @@
     <!-- Custom CSS -->
     <link href="../assets/extra-libs/c3/c3.min.css" rel="stylesheet">
     <link href="../assets/libs/chartist/dist/chartist.min.css" rel="stylesheet">
-     <!-- This page plugin CSS -->
-     <link href="../assets/extra-libs/datatables.net-bs4/css/dataTables.bootstrap4.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="../dist/css/style.min.css" rel="stylesheet">
 
+    <!-- <script type="text/javascript">
+    function valid(){
+        if(document.registration.password.value!= document.registration.cpassword.value){
+            alert("Password and Re-Type Password Field do not match !!");
+            document.registration.cpassword.focus();
+        return false;
+            } return true;
+     }
+    </script> -->
+    
 </head>
 
 <body>
@@ -46,7 +59,7 @@
     <!-- ============================================================== -->
     <div class="preloader">
         <div class="lds-ripple">
-            <div class="lds-pos"></div>
+            <div class="lds-pos">dfghjjhgfd</div>
             <div class="lds-pos"></div>
         </div>
     </div>
@@ -59,7 +72,7 @@
         <!-- Topbar header - style you can find in pages.scss -->
         <!-- ============================================================== -->
         <header class="topbar" data-navbarbg="skin6">
-            <?php include 'includes/navigation.php'?>
+            <?php include '../includes/student-navigation.php'?>
         </header>
         <!-- ============================================================== -->
         <!-- End Topbar header -->
@@ -70,7 +83,7 @@
         <aside class="left-sidebar" data-sidebarbg="skin6">
             <!-- Sidebar scroll-->
             <div class="scroll-sidebar" data-sidebarbg="skin6">
-                <?php include 'includes/sidebar.php'?>
+                <?php include '../includes/student-sidebar.php'?>
             </div>
             <!-- End Sidebar scroll-->
         </aside>
@@ -81,37 +94,22 @@
         <!-- Page wrapper  -->
         <!-- ============================================================== -->
         <div class="page-wrapper">
-            <!-- ============================================================== -->
-            <!-- Bread crumb and right sidebar toggle -->
-            <!-- ============================================================== -->
-            <div class="page-breadcrumb">
-                <div class="row">
-                    <div class="col-7 align-self-center">
-                    <h4 class="page-title text-truncate text-dark font-weight-medium mb-1">My Profile</h4>
-                        <div class="d-flex align-items-center">
-                            <h6 class="card-subtitle"><code>You cannot make changes in username and registered date!</code> </h6> 
-                        </div>
-                    </div>
-                    
-                </div>
-
-            </div>
-            <!-- ============================================================== -->
-            <!-- End Bread crumb and right sidebar toggle -->
-            <!-- ============================================================== -->
+            <!-- By CodeAstro - codeastro.com -->
             <!-- ============================================================== -->
             <!-- Container fluid  -->
             <!-- ============================================================== -->
             <div class="container-fluid">
+                
+                <div class="col-7 align-self-center">
+                        <h4 class="page-title text-truncate text-dark font-weight-medium mb-1">My Profile</h4>
+                </div>
 
-                <form method="POST">
 
-                    <div class="row">
-
+                <div class="row">
 
                     <?php	
                     $aid=$_SESSION['id'];
-                        $ret="SELECT * from admin where id=?";
+                        $ret="select * from userregistration where id=?";
                             $stmt= $mysqli->prepare($ret) ;
                         $stmt->bind_param('i',$aid);
                         $stmt->execute() ;//ok
@@ -120,14 +118,73 @@
                         while($row=$res->fetch_object())
                         {
                             ?>
+    
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Last Updated On</h4>
+                                        <div class="form-group">
+                                            <input type="text" value="<?php echo $row->updationDate; ?>" class="form-control" required readonly>
+                                        </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+<!-- By CodeAstro - codeastro.com -->
+
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Registration Number</h4>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" value="<?php echo $row->regNo;?>" required readonly>
+                                        </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+
+                </div>
+                
+                    
+                    <form name="registration" onSubmit="return valid();" method="POST">
+
+                    <div class="row">
+                    
+                        <div class="col-sm-12 col-md-6 col-lg-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">First Name</h4>
+                                        <div class="form-group">
+                                            <input type="text" name="fname" id="fname" class="form-control" value="<?php echo $row->firstName;?>"   required="required">
+                                        </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
 
 
                         <div class="col-sm-12 col-md-6 col-lg-4">
                             <div class="card">
                                 <div class="card-body">
-                                    <h4 class="card-title">Username</h4>
+                                    <h4 class="card-title">Middle Name</h4>
                                         <div class="form-group">
-                                            <input type="text" value="<?php echo $row->username;?>" disabled class="form-control">
+                                            <input type="text" name="mname" id="mname" class="form-control" value="<?php echo $row->middleName;?>">
+                                        </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-sm-12 col-md-6 col-lg-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Last Name</h4>
+                                        <div class="form-group">
+                                            <input type="text" name="lname" id="lname" class="form-control" value="<?php echo $row->lastName;?>" required="required">
                                         </div>
                                     
                                 </div>
@@ -137,13 +194,17 @@
 
 
                         <div class="col-sm-12 col-md-6 col-lg-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h4 class="card-title">Email ID</h4>
-                                        <div class="form-group">
-                                            <input type="email" class="form-control" name="emailid" id="emailid" value="<?php echo $row->email;?>" required="required">
-                                        </div>
-                                    
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Gender</h4>
+                                    <div class="form-group mb-4">
+                                        <select class="custom-select mr-sm-2" id="gender" name="gender">
+                                            <option value="<?php echo $row->gender;?>"><?php echo $row->gender;?></option>
+                                            <option value="Male">Male</option>
+                                            <option value="Female">Female</option>
+                                            <option value="Others">Others</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -152,31 +213,41 @@
                         <div class="col-sm-12 col-md-6 col-lg-4">
                             <div class="card">
                                 <div class="card-body">
-                                    <h4 class="card-title">Registered Account On</h4>
+                                    <h4 class="card-title">Email Address</h4>
                                         <div class="form-group">
-                                            <input type="text" class="form-control" value="<?php echo $row->reg_date;?>" disabled>
+                                            <input type="email" name="email" id="email" class="form-control" value="<?php echo $row->email;?>" readonly>
                                         </div>
                                 </div>
                             </div>
                         </div>
 
-                        <h6 class="card-subtitle"><code>Last Updated On: </code> <?php echo $row->updation_date;?> </h6>
+<!-- By CodeAstro - codeastro.com -->
+
+                        <div class="col-sm-12 col-md-6 col-lg-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Contact Number</h4>
+                                        <div class="form-group">
+                                            <input type="text" name="contact" id="contact" maxlength="10" class="form-control" value="<?php echo $row->contactNo;?>" required="required">
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
 
                         <?php } ?>
-
+                    
                     </div>
-
 
                         <div class="form-actions">
                             <div class="text-center">
                                 <button type="submit" name="update" class="btn btn-success">Make Changes</button>
-                                <button type="reset" class="btn btn-danger">Reset</button>
                             </div>
                         </div>
 
-                 
-                 </form>
-                 
+                    </form>
+
+
+
             </div>
             <!-- ============================================================== -->
             <!-- End Container fluid  -->
@@ -203,7 +274,7 @@
     <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
     <script src="../assets/libs/popper.js/dist/umd/popper.min.js"></script>
     <script src="../assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- apps -->
+    <!-- apps --><!-- By CodeAstro - codeastro.com -->
     <!-- apps -->
     <script src="../dist/js/app-style-switcher.js"></script>
     <script src="../dist/js/feather.min.js"></script>
@@ -217,9 +288,6 @@
     <script src="../assets/libs/chartist/dist/chartist.min.js"></script>
     <script src="../assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
     <script src="../dist/js/pages/dashboards/dashboard1.min.js"></script>
-    <script src="../assets/extra-libs/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="../dist/js/pages/datatable/datatable-basic.init.js"></script>
-
 </body>
 
 </html>
